ba1667260259c84bfd9cb1836300d5433f72c059
Prem Burnwal <premburnwal31@gmail.com>
2026-04-28 23:31:47 +0530

    feat: add rate limiting filter

---
+++ SocioCircle/backend/src/main/java/com/example/SocioCircle/Security/RateLimitFilter.java
@@ -0,0 +1,27 @@
+package com.example.SocioCircle.Security;
+
+import com.example.SocioCircle.Service.RateLimitService;
+import io.github.bucket4j.Bucket;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class RateLimitFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private RateLimitService rateLimitService;
+
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
